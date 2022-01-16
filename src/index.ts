b8a2f6f8b7e9e7e8f136a15d4a866374d85e3cb1
ndaemy <yuyaebean@gmail.com>
2022-01-16 22:44:12 +0900

    feat: 초기 세팅

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
