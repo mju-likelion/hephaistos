62d250ad56a6015756e4dc8fab888b9caa4c2345
ndaemy <yuyaebean@gmail.com>
2022-01-25 01:55:10 +0900

    feat: sequelize 이용해서 데이터베이스 연결

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
+import dotenv from "dotenv";
 import express from "express";
+import { Sequelize } from "sequelize";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || "postgresql://localhost:5432/hephaistos",
+);
+
+app.get("/", async (req, res) => {
   res.send("hello world");
 });
 
+app.get("/db-healthcheck", async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.send("Connection has been established successfully.");
+  } catch (error) {
+    res.send(`Unable to connect to the database: ", ${error}`);
+  }
+});
+
 app.listen(port, () => {
+  /* eslint-disable no-console */
   console.log(`App listening at http://localhost:${port}`);
 });
