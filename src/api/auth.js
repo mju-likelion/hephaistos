807410c4e47ab4a7cc37e35605cb762e62a687d7
Crmal <pjm2207@naver.com>
2022-01-27 08:04:41 +0900

    fix: html url 수정

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,5 @@
 import crypto from "crypto";
+// import { sign } from "jsonwebtoken";
 
 import express from "express";
 import nodemailer from "nodemailer";
@@ -9,19 +10,7 @@ const router = express();
 router.post("/email-verify", async (req, res) => {
   // 해시코드 생성
   const code = crypto.randomBytes(6).toString("hex");
-
-  let emailTemplete; // 보낼 메일
-
-  res.render("src/views/emailTest.html", { authCode: code }, (err, data) => {
-    if (err) {
-      res.json({
-        error: {
-          message: "이메일 형식이 올바르지 않습니다.",
-        },
-      });
-    }
-    emailTemplete = data;
-  });
+  // redis 코드 저장
 
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -39,9 +28,9 @@ router.post("/email-verify", async (req, res) => {
       from: `pjm2207@likelion.org`,
       to: req.body.email,
       subject: "멋쟁이사자처럼 10기 이메일인증",
-      html: emailTemplete,
+      html: `<a href=http://localhost:3000/api/auth/email-verify/${code}>인증하기</a>`,
     },
-    (err: any) => {
+    err => {
       if (err) {
         res.status(400).json({
           error: {
@@ -58,10 +47,12 @@ router.post("/email-verify", async (req, res) => {
 // 이메일인증(POST /api/auth/email-verify/:emailToken )
 // router.post('auth/email-verify/:emailToken', (req, res, next) => {
 //   const emailToken = req.params.emailToken;
+// redis 안에 값과 비교
+// res
 // });
 // 회원가입(POST /api/auth/sign-up)
 // router.post('auth/sign-up', (req, res, next) => {
-
+//  (password, name, phonenumber, school, major) 비구조
 // });
 
 export default router;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import authRouter from "./api/auth.ts";
+import authRouter from "./api/auth";
 import models from "./models";
 
 const app = express();
