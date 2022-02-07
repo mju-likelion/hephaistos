263e454d668912d52d292003c1f7b31cb562528b
ndaemy <yuyaebean@gmail.com>
2022-02-08 01:54:43 +0900

    feat: 데이터베이스 스펙 업데이트 및 리팩토링

--- src/models/apply.js
+++ src/models/apply.js
@@ -1,51 +1,48 @@
-import Sequelize from "sequelize";
+import { DataTypes, Model } from "sequelize";
 
-export default class Apply extends Sequelize.Model {
+class Apply extends Model {
   static init(sequelize) {
     return super.init(
       {
         part: {
-          type: Sequelize.ENUM("design", "development", "manage"),
+          type: DataTypes.ENUM("design", "web", "server"),
           allowNull: false,
         },
         one: {
-          type: Sequelize.STRING(1500),
+          type: DataTypes.STRING(1500),
         },
         two: {
-          type: Sequelize.STRING(1500),
+          type: DataTypes.STRING(1500),
         },
         three: {
-          type: Sequelize.STRING(1500),
+          type: DataTypes.STRING(1500),
         },
         four: {
-          type: Sequelize.STRING(1500),
+          type: DataTypes.STRING(1500),
         },
         five: {
-          type: Sequelize.STRING(1500),
+          type: DataTypes.STRING(1500),
         },
         six: {
-          type: Sequelize.STRING(1500),
+          type: DataTypes.STRING(1500),
         },
         seven: {
-          type: Sequelize.STRING(1500),
+          type: DataTypes.STRING(1500),
         },
         eight: {
-          type: Sequelize.STRING(1500),
+          type: DataTypes.STRING(1500),
         },
         nine: {
-          type: Sequelize.STRING(1500),
+          type: DataTypes.STRING(1500),
         },
         ten: {
-          type: Sequelize.STRING(1500),
+          type: DataTypes.STRING(1500),
         },
       },
       {
         sequelize,
-        timestamps: true,
-        underscored: false,
-        paranoid: false,
         modelName: "Apply",
-        tableName: "applys",
+        tableName: "apply",
         charset: "utf8mb4",
         collate: "utf8mb4_general_ci",
       },
@@ -53,6 +50,11 @@ export default class Apply extends Sequelize.Model {
   }
 
   static associate(db) {
-    db.Apply.belongsTo(db.User, { foreignKey: "UserId", sourceKey: "id" });
+    db.Apply.belongsTo(db.User, {
+      foreignKey: { name: "userId", allowNull: false },
+      sourceKey: "id",
+    });
   }
 }
+
+export default Apply;
--- src/models/index.js
+++ src/models/index.js
@@ -4,34 +4,28 @@ import { Sequelize } from "sequelize";
 import Admin from "./admin";
 import Apply from "./apply";
 import Question from "./question";
-import Univ from "./univ";
 import User from "./user";
 
 dotenv.config();
-
-const db = {};
 
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || "postgresql://localhost:5432/hephaistos",
 );
 
-db.sequelize = sequelize;
-db.User = User;
-db.Admin = Admin;
-db.Apply = Apply;
-db.Question = Question;
-db.Univ = Univ;
+const db = {
+  sequelize,
+  User,
+  Admin,
+  Apply,
+  Question,
+};
 
 User.init(sequelize);
 Admin.init(sequelize);
 Apply.init(sequelize);
 Question.init(sequelize);
-Univ.init(sequelize);
 
 User.associate(db);
-Admin.associate(db);
 Apply.associate(db);
-Question.associate(db);
-Univ.associate(db);
 
 export default db;
