7d6c1f1265dde1a344d6c5030cea0a32474783e0
minjung <ypd06021@naver.com>
2022-02-14 18:47:51 +0900

    refactor: api/index에 apply 추가

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
+import applyRouter from "./apply";
 import authRouter from "./auth";
 
 const api = Router();
 
+api.use("/apply", applyRouter);
 api.use("/auth", authRouter);
 
 export default api;
