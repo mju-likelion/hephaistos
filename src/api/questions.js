9ac756f7e2ca48e8acef751cab6c1d2658471073
Crmal <pjm2207@naver.com>
2022-02-15 02:16:55 +0900

    feat: questions 라우터 추가

--- src/api/questions.js
+++ src/api/questions.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
 
+import Question from "../models/question";
 // import Question from "../models/question";
 
 const questions = Router();
 
 // 지원서 질문
 questions.get("/", async (req, res) => {
-  res.json("test");
+  // ['one','two','three','four','five','six','seven','eight','nine','ten']
+  const question = await Question.findOne({
+    attributes: ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"],
+  });
+  res.json({
+    data: {
+      questions: Object.values(question.dataValues),
+    },
+  });
 });
 
 export default questions;
