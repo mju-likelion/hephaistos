f78cfdd955ec08edcc66615b77f66a8d0ce1349e
Crmal <pjm2207@naver.com>
2022-02-17 11:00:24 +0900

    feat: where defalt값 추가

--- src/api/apply.js
+++ src/api/apply.js
@@ -6,6 +6,7 @@ import { verify } from "jsonwebtoken";
 import { omit } from "lodash";
 import nodemailer from "nodemailer";
 
+import { loginChecker, adminChecker } from "../middleware/checker";
 import { submitValidator } from "../middleware/validator";
 import Apply from "../models/apply";
 import User from "../models/user";
@@ -13,138 +14,80 @@ import User from "../models/user";
 const apply = Router();
 dotenv.config();
 // 지원서 전체보기
-apply.get("/", async (req, res) => {
-  try {
-    const token = verify(req.header("X-Access-Token"), process.env.JWT_SECRET);
-    if (token?.admin) {
-      const { query } = parse(req.url, true);
-      const users = await User.findAll({
-        include: {
-          model: Apply,
-          attributes: [],
-          where: { part: query.part },
-        },
-        attributes: ["id", "email", "name", "major"],
-        where: {
-          status: query.status.split(":"),
-        },
-        order: [query.sort.split("_")],
-        limit: query?.size ? query.size : 10,
-        offset: query?.page ? (query.page - 1) * (query?.size ? query.size : 10) : 1,
-      });
-      const userCount = await User.count({
-        include: {
-          model: Apply,
-          attributes: [],
-          where: { part: query.part },
-        },
-        where: {
-          status: query.status.split(":"),
-        },
-      });
-      return res.json({
-        links: {
-          prev_uri:
-            query?.page && !(query?.page === "1")
-              ? `/applylists?${parse(req.url, false).query.replace(
-                  `&page=${query.page}`,
-                  `&page=${+query.page - 1}`,
-                )}`
-              : null,
-          next_uri:
-            userCount / (query?.page ? query.page * (query?.size ? query.size : 10) : 1) > 1
-              ? `/applylists?${parse(req.url, false).query.replace(
-                  `&page=${query.page}`,
-                  `&page=${+query.page + 1}`,
-                )}`
-              : null,
-        },
-        data: {
-          users,
-        },
-      });
-    }
-    return res.status(403).json({
-      error: {
-        message: "권한이 없습니다.",
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      error: {
-        message: "알수없는 에러가 발생하였습니다.",
-      },
-    });
-  }
+apply.get("/", loginChecker, adminChecker, async (req, res) => {
+  const { query } = parse(req.url, true);
+  const users = await User.findAll({
+    include: {
+      model: Apply,
+      attributes: [],
+      where: { part: query?.part ? query.part.split(":") : ["design", "web", "server"] },
+    },
+    attributes: ["id", "email", "name", "major"],
+    where: {
+      status: query?.status
+        ? query.status.split(":")
+        : ["complete", "first-fail", "first-pass", "second-fail", "second-pass"],
+    },
+    order: [query.sort.split("_")],
+    limit: query?.size ? query.size : 10,
+    offset: query?.page ? (query.page - 1) * (query?.size ? query.size : 10) : 0,
+  });
+  const userCount = await User.count({
+    include: {
+      model: Apply,
+      attributes: [],
+      where: { part: query?.part ? query.part.split(":") : ["design", "web", "server"] },
+    },
+    where: {
+      status: query?.status
+        ? query.status.split(":")
+        : ["complete", "first-fail", "first-pass", "second-fail", "second-pass"],
+    },
+  });
+  return res.json({
+    links: {
+      prev_uri:
+        query?.page && !(query?.page === "1")
+          ? `/applylists?${parse(req.url, false).query.replace(
+              `&page=${query.page}`,
+              `&page=${+query.page - 1}`,
+            )}`
+          : null,
+      next_uri:
+        userCount / (query?.page ? query.page * (query?.size ? query.size : 10) : 1) > 1
+          ? `/applylists?${parse(req.url, false).query.replace(
+              `&page=${query.page}`,
+              `&page=${+query.page + 1}`,
+            )}`
+          : null,
+    },
+    data: {
+      users,
+    },
+  });
 });
 
 // 지원서 전체 개수
-apply.get("/total-count", async (req, res) => {
-  try {
-    const token = verify(req.header("X-Access-Token"), process.env.JWT_SECRET);
-    if (token?.admin) {
-      return res.json({
-        mata: {
-          count: await Apply.count(),
-        },
-      });
-    }
-    return res.status(403).json({
-      error: {
-        message: "권한이 없습니다",
-      },
-    });
-  } catch (error) {
-    return res.status(500).json({
-      error: {
-        message: "알수없는 에러가 발생하였습니다.",
-      },
-    });
-  }
+apply.get("/total-count", loginChecker, adminChecker, async (req, res) => {
+  res.json({
+    mata: {
+      count: await Apply.count(),
+    },
+  });
 });
 
 // 지원자 보기
-apply.get("/:id", async (req, res) => {
-  try {
-    // eslint-disable-next-line
-    const { id } = req.params;
-    const token = verify(req.header("X-Access-Token"), process.env.JWT_SECRET);
-    if (token?.admin) {
-      const user = await User.findOne({
-        attributes: ["id", "email", "phone", "name", "major", "status"],
-        where: { id },
-      });
-      const applyData = await Apply.findOne({ where: { userId: id } });
-      if (!user || !applyData) {
-        return res.status(403).json({
-          error: {
-            message: "요청이 올바르지 않습니다.",
-          },
-        });
-      }
-      return res.json({
-        data: user,
-        apply: {
-          part: applyData.part,
-          answer: Object.values(
-            omit(applyData.dataValues, [
-              "id",
-              "applyVerify",
-              "part",
-              "createdAt",
-              "updatedAt",
-              "userId",
-            ]),
-          ),
-        },
-      });
-    }
+apply.get("/:id", loginChecker, async (req, res) => {
+  // eslint-disable-next-line
+  const { id } = req.params;
+  const token = verify(req.header("X-Access-Token"), process.env.JWT_SECRET);
+  if (token?.isAdmin) {
     const user = await User.findOne({
-      attributes: ["id", "email", "phone", "name", "major"],
-      where: { email: token.email },
+      attributes: ["id", "email", "phone", "name", "major", "status"],
+      where: { id },
     });
     const applyData = await Apply.findOne({ where: { userId: user.id } });
-    if (!applyData) {
+    if (!user || !applyData) {
       return res.status(403).json({
         error: {
           message: "요청이 올바르지 않습니다.",
@@ -163,60 +106,60 @@ apply.get("/:id", async (req, res) => {
             "createdAt",
             "updatedAt",
             "userId",
-            "status",
           ]),
         ),
       },
     });
-  } catch (error) {
-    return res.status(500).json({
+  }
+  const user = await User.findOne({
+    attributes: ["id", "email", "phone", "name", "major"],
+    where: { id: token.id },
+  });
+  const applyData = await Apply.findOne({ where: { userId: user.id } });
+  if (!applyData) {
+    return res.status(403).json({
       error: {
-        message: "알수없는 에러가 발생하였습니다.",
+        message: "요청이 올바르지 않습니다.",
       },
     });
   }
+  return res.json({
+    data: user,
+    apply: {
+      part: applyData.part,
+      answer: Object.values(
+        omit(applyData.dataValues, [
+          "id",
+          "applyVerify",
+          "part",
+          "createdAt",
+          "updatedAt",
+          "userId",
+          "status",
+        ]),
+      ),
+    },
+  });
 });
 
 // 임시저장
-apply.put("/", async (req, res) => {
-  try {
-    // eslint-disable-next-line
-    const applyData = req.body.data.apply;
-    const token = verify(req.header("X-Access-Token"), process.env.JWT_SECRET);
-    const user = await User.findOne({ where: { email: token.email } });
-    const applyCheck = await Apply.findOne({ where: { userId: user.id } });
-    if (applyCheck) {
-      if (applyCheck.applyVerify) {
-        return res.status(403).json({
-          error: {
-            message: "이미 제출이 완료된 상태입니다.",
-          },
-        });
-      }
-      await Apply.update({
-        part: applyData.part,
-        one: applyData.one,
-        two: applyData.two,
-        three: applyData.three,
-        four: applyData.four,
-        five: applyData.five,
-        six: applyData.six,
-        seven: applyData.seven,
-        eight: applyData.eight,
-        nine: applyData.nine,
-        ten: applyData.ten,
-      });
-      return res.json({
-        data: {
-          message:
-            "지원서가 임시저장 되었습니다. 임시저장만으로는 제출되지않습니다. 최종 제출 이후에는 확인 및 수정이 불가능 합니다.",
+apply.put("/", loginChecker, async (req, res) => {
+  // eslint-disable-next-line
+  const applyData = req.body.data.apply;
+  const token = verify(req.header("X-Access-Token"), process.env.JWT_SECRET);
+  const user = await User.findOne({ where: { id: token.id } });
+  const applyCheck = await Apply.findOne({ where: { userId: user.id } });
+  if (applyCheck) {
+    if (applyCheck.applyVerify) {
+      return res.status(403).json({
+        error: {
+          message: "이미 제출이 완료된 상태입니다.",
         },
       });
     }
-    await Apply.create({
-      applyVerify: false,
+    await Apply.update({
       part: applyData.part,
-      one: applyData.part,
+      one: applyData.one,
       two: applyData.two,
       three: applyData.three,
       four: applyData.four,
@@ -226,7 +169,6 @@ apply.put("/", async (req, res) => {
       eight: applyData.eight,
       nine: applyData.nine,
       ten: applyData.ten,
-      userId: user.id,
     });
     return res.json({
       data: {
@@ -234,69 +176,56 @@ apply.put("/", async (req, res) => {
           "지원서가 임시저장 되었습니다. 임시저장만으로는 제출되지않습니다. 최종 제출 이후에는 확인 및 수정이 불가능 합니다.",
       },
     });
-  } catch (error) {
-    return res.status(500).json({
-      error: {
-        message: "알수없는 에러가 발생하였습니다.",
-      },
-    });
   }
+  await Apply.create({
+    applyVerify: false,
+    part: applyData.part,
+    one: applyData.part,
+    two: applyData.two,
+    three: applyData.three,
+    four: applyData.four,
+    five: applyData.five,
+    six: applyData.six,
+    seven: applyData.seven,
+    eight: applyData.eight,
+    nine: applyData.nine,
+    ten: applyData.ten,
+    userId: user.id,
+  });
+  return res.json({
+    data: {
+      message:
+        "지원서가 임시저장 되었습니다. 임시저장만으로는 제출되지않습니다. 최종 제출 이후에는 확인 및 수정이 불가능 합니다.",
+    },
+  });
 });
 
 // 제출
-apply.post("/", submitValidator, async (req, res) => {
-  try {
-    // eslint-disable-next-line
-    const applyData = req.body.data.apply;
-    const token = verify(req.header("X-Access-Token"), process.env.JWT_SECRET);
-    const user = await User.findOne({ where: { email: token.email } });
-    const applyCheck = await Apply.findOne({ where: { userId: user.id } });
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.NODEMAILER_USER,
-        pass: process.env.NODEMAILER_PASS,
-      },
-    });
-    if (applyCheck) {
-      if (applyCheck.applyVerify) {
-        return res.status(403).json({
-          error: {
-            message: "이미 제출이 완료된 상태입니다.",
-          },
-        });
-      }
-      await Apply.update({
-        applyVerify: true,
-        part: applyData.part,
-        one: applyData.part,
-        two: applyData.two,
-        three: applyData.three,
-        four: applyData.four,
-        five: applyData.five,
-        six: applyData.six,
-        seven: applyData.seven,
-        eight: applyData.eight,
-        nine: applyData.nine,
-        ten: applyData.ten,
-      });
-      transporter.sendMail({
-        from: `mju@likelion.org`,
-        to: user.email,
-        subject: "멋쟁이사자처럼 10기 지원확인 메일",
-        html: `<h1>지원해주셔서 감사합니다. ...</h1>`,
-      });
-      return res.json({
-        data: {
-          message:
-            "지원서가 제출되었습니다. 가입된 이메일로 지원확인 메일이 발송되었습니다. 최종 제출 이후 확인 및 수정이 불가능 합니다",
+apply.post("/", loginChecker, submitValidator, async (req, res) => {
+  // eslint-disable-next-line
+  const applyData = req.body.data.apply;
+  const token = verify(req.header("X-Access-Token"), process.env.JWT_SECRET);
+  const user = await User.findOne({ where: { id: token.id } });
+  const applyCheck = await Apply.findOne({ where: { userId: user.id } });
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASS,
+    },
+  });
+  if (applyCheck) {
+    if (applyCheck.applyVerify) {
+      return res.status(403).json({
+        error: {
+          message: "이미 제출이 완료된 상태입니다.",
         },
       });
     }
-    await Apply.create({
+    await Apply.update({
       applyVerify: true,
       part: applyData.part,
       one: applyData.part,
@@ -309,11 +238,10 @@ apply.post("/", submitValidator, async (req, res) => {
       eight: applyData.eight,
       nine: applyData.nine,
       ten: applyData.ten,
-      userId: user.id,
     });
     transporter.sendMail({
       from: `mju@likelion.org`,
-      to: user.email, // user.email로 변경
+      to: user.email,
       subject: "멋쟁이사자처럼 10기 지원확인 메일",
       html: `<h1>지원해주셔서 감사합니다. ...</h1>`,
     });
@@ -323,12 +251,33 @@ apply.post("/", submitValidator, async (req, res) => {
           "지원서가 제출되었습니다. 가입된 이메일로 지원확인 메일이 발송되었습니다. 최종 제출 이후 확인 및 수정이 불가능 합니다",
       },
     });
-  } catch (error) {
-    return res.status(500).json({
-      error: {
-        message: "알수없는 에러가 발생하였습니다.",
-      },
-    });
   }
+  await Apply.create({
+    applyVerify: true,
+    part: applyData.part,
+    one: applyData.part,
+    two: applyData.two,
+    three: applyData.three,
+    four: applyData.four,
+    five: applyData.five,
+    six: applyData.six,
+    seven: applyData.seven,
+    eight: applyData.eight,
+    nine: applyData.nine,
+    ten: applyData.ten,
+    userId: user.id,
+  });
+  transporter.sendMail({
+    from: `mju@likelion.org`,
+    to: user.email, // user.email로 변경
+    subject: "멋쟁이사자처럼 10기 지원확인 메일",
+    html: `<h1>지원해주셔서 감사합니다. ...</h1>`,
+  });
+  return res.json({
+    data: {
+      message:
+        "지원서가 제출되었습니다. 가입된 이메일로 지원확인 메일이 발송되었습니다. 최종 제출 이후 확인 및 수정이 불가능 합니다",
+    },
+  });
 });
 export default apply;
