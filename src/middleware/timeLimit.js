eea4f4027ec9419949702adaef02f18b6fe7409f
Ubuntu <ubuntu@ip-172-31-36-106.ap-northeast-2.compute.internal>
2022-03-14 09:33:49 +0000

    fix: timeLimit시간 변경

--- src/middleware/timeLimit.js
+++ src/middleware/timeLimit.js
@@ -2,7 +2,7 @@ import { compareAsc } from "date-fns";
 
 export function dateChecker(req, res, next) {
   // 두 날짜를 비교하고 첫 번째 날짜가 두 번째 날짜 이후이면 1, 이전이면 -1, 날짜가 같으면 0을 반환
-  if (compareAsc(new Date(), new Date(2022, 2, 12, 0, 30)) === 1) {
+  if (compareAsc(new Date(), new Date(2022, 2, 11, 13, 30)) === 1) {
     return res.status(403).json({
       error: {
         message: "지원기간이 끝났습니다.",
